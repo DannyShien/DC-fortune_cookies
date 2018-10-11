7a2dca188625081b6adba842049388bd5ef47b97
Stanley Chen <dannyshien@gmail.com>
2018-10-11 09:54:04 -0400

    add index.html

---
+++ script/index.js
@@ -0,0 +1,14 @@
+console.log('yep, its here');
+// Classes are for CSS Styling
+// 'Data Attributes' are for JS
+
+// 1. Make some code run when I click the button
+const triggerElement = document.querySelector('[data-trigger]');
+
+// addEventListener accepts 2 arguments: 
+// - a string that identifies the kind of event to listen for
+// - a function that says what to do when the event happens
+triggerElement.addEventListener('click', );
+// 2. Write some text to the page on the buttom click
+
+// 3. Pull text from an array of text to write to the page.
